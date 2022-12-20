a5376332eb876c464e9d28267b07e5e553709e6e
Vinicius <viniciusmaiamarinho1@gmail.com>
2022-12-20 11:47:40 -0300

    feat: add movie route

---
+++ src/routes/movie.routes.ts
@@ -0,0 +1,13 @@
+import { Router } from "express";
+import { CreateMovieController } from "../modules/movies/movieCase/createMovie/CreateMovieController";
+import { CreateMovieRentController } from "../modules/movies/movieCase/createMovieRent/CreateMovieRentController";
+
+const createMovieController = new CreateMovieController();
+const createMovieRentController = new CreateMovieRentController();
+
+const movieRoutes = Router();
+
+movieRoutes.post("/", createMovieController.handle);
+movieRoutes.post("/rent", createMovieRentController.handle);
+
+export {movieRoutes};
