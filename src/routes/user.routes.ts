e9e605451a8fa71c5d3b3629954a2b9583eef7a1
Vinicius <viniciusmaiamarinho1@gmail.com>
2022-12-21 13:02:36 -0300

    chore: add rotas de get all

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,10 +1,13 @@
 import { Router } from "express";
-import { CreateUserController } from "../modules/users/userCase/CreateUserCaseController";
+import { CreateUserController } from "../modules/users/useCase/createUser/CreateUserCaseController"
+import { GetAllUsersController } from "../modules/users/useCase/getAllUsers/GetAllUsersController";
 
 const createUserController = new CreateUserController();
+const getAllUsersController = new GetAllUsersController();
 
 const userRoutes = Router();
 
 userRoutes.post("/", createUserController.handle);
+userRoutes.get("/", getAllUsersController.handle);
 
 export {userRoutes};
